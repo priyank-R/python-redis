8b98beef3f345904bacf7e124ec452f5abdf65be
priyank-R <piyurupareliya@gmail.com>
2026-06-17 12:03:11 +0530

    Add support for PING command

---
+++ core/eval.py
@@ -0,0 +1,42 @@
+from core.cmd import RedisCmd
+import socket
+import logging
+
+logger = logging.getLogger()
+
+
+
+def Encode(o: object, isSimple: bool) -> list[bytes]:
+    if isinstance(o, str): 
+        if isSimple: 
+            return f"+{o}\r\n".encode()
+        else:
+            return f"${len(o)}\r\n{o}\r\n".encode()
+        
+    return bytearray()
+
+def evalPING(args: list[str], client: socket.socket):
+    if len(args) >= 2:
+        return Exception("ERR wrong number of arguments for 'ping' command")
+    
+    if len(args) == 0:
+        b = Encode("PONG", True)
+    else:
+        b = Encode(args[0], False)
+    try: 
+        client.sendall(b)
+    except Exception as e:
+        logger.error(e)
+        return e        
+
+
+def evalAndRespond(cmd: RedisCmd, client: socket.socket) -> Exception | None:
+    command = cmd.cmd
+    args = cmd.args
+
+    logger.info(f"evalAndRespond args {args}")
+
+    if command.upper() == 'PING':
+        return evalPING(args, client)
+    
+    evalPING([], client)
